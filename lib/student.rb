d003ab2c5510ef5ce2be82f8c000f92ea143f4b8
David Kuruvilla <dapawn@gmail.com>
2018-01-10 15:50:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,4 +6,19 @@ class Student < InteractiveRecord
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
+
+  def self.find_by(hash)
+    hash.each.with_index do |key,value|
+      sql = <<-SQL
+        SELECT * FROM #{self.table_name} WHERE ? = ?
+      SQL
+
+      rows = DB[:conn].execute(sql, key, value)
+      if rows.first
+        self.reify_from_row(rows.first)
+      else
+        nil
+      end
+    end
+  end
 end
